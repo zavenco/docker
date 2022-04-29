88148f62f0e4d44ffed357eb2f498243123433c3
Bartosz Gasztych <bartosz.gasztych@zaven.pl>
2022-04-29 11:40:01 +0200

    add cache

--- flutter/build_docker.sh
+++ flutter/build_docker.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-docker buildx build --tag zavenco/flutter:${FLUTTER_VERSION/+/-} \
+docker build --cache-from zavenco/flutter:${FLUTTER_VERSION/+/-} \
+             --tag zavenco/flutter:${FLUTTER_VERSION/+/-} \
              --tag zavenco/flutter:$DOCKER_TAG \
              --build-arg flutter_version=$FLUTTER_VERSION \
-             --platform=linux/arm64,linux/amd64 \
              --progress=plain \
              .
