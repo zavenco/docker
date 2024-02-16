e9477c7a18fd6bf82bf155c66a3950873d986cd2
Bartosz Gasztych <bartosz.gasztych@zaven.pl>
2024-02-16 13:51:34 +0100

    chore: flutter 3.16.9, openjdk19, ubuntu:jammy

--- flutter/build_docker.sh
+++ flutter/build_docker.sh
@@ -5,5 +5,4 @@ set -e
 docker build --cache-from zavenco/flutter:${FLUTTER_VERSION/+/-} \
              --tag zavenco/flutter:${FLUTTER_VERSION/+/-} \
              --build-arg flutter_version=$FLUTTER_VERSION \
-             --progress=plain \
              .
